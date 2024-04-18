00ca69a0edaac6cef298a1d497324801d8a0a559
Abhinav Krishna <abhinavkrishnacr2020@gmail.com>
2024-04-18 21:44:14 +0530

    Rewrite code to use Iterator

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,43 @@
-use std::error::Error;
+use std::{error::Error, io::Write, iter::repeat};
 
-use image::{io::Reader as ImageReader, GenericImageView, Pixel, Rgb, Rgba};
+use image::{io::Reader as ImageReader, DynamicImage, GenericImageView, Rgba};
 
 const BRIGHTNESS_CHARS: &str =
-    r#"$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,"^`'. "#;
+    r#" .'`^",:;Il!i><~+_-?][}{1)(|\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"#;
 const BRAINFUCK_CHARS: &str = "<>[]+-,.";
 
 // TODO: Remove BRAINFUCK_CHARS
-fn get_char_by_brightness(brightness: u8) -> u8 {
-  let i = (brightness as usize * (BRIGHTNESS_CHARS.len() - 1)) / 255;
-  // Safe because maximum possible value is  BRIGHTNESS_CHARS.len() - 1
-  *BRIGHTNESS_CHARS.as_bytes().get(i).unwrap()
+fn get_pixel_char(brightness: u8) -> char {
+    let i = (brightness as usize * (BRIGHTNESS_CHARS.len() - 1)) / 255;
+    // Safe because maximum possible value is  BRIGHTNESS_CHARS.len() - 1
+    char::from(*BRIGHTNESS_CHARS.as_bytes().get(i).unwrap())
+}
+fn pixel_brightness(Rgba([r, g, b, a]): Rgba<u8>) -> u8 {
+    let avg_color = (r as u32 * g as u32 * b as u32) / 3;
+    ((avg_color * a as u32) / 255) as u8
 }
 
-fn read_image(path: &str) -> Result<(), Box<dyn Error>> {
-    let required_width = 300;
-
-    let img = ImageReader::open(path)?.decode()?.grayscale();
+fn image_ascii_chars(img: DynamicImage, required_width: u32) -> impl Iterator<Item = char> {
     let (w, h) = img.dimensions();
 
     // NOTE: Higher scale means smaller image
     let scale = w / required_width;
-    for y in 0..h {
-        for x in 0..w {
-            if y % (scale * 2) == 0 && x % scale == 0 {
-              let Rgba([brightness, _, _, _]) = img.get_pixel(x, y);
-              print!("{}", char::from(get_char_by_brightness(brightness)));
-            }
-        }
-        if y % (scale * 2) == 0 {
-          println!("")
-        }
-    }
-    Ok(())
+    (0..h)
+        .filter(move |y| y % (scale * 3) == 0)
+        .zip(repeat(img))
+        .flat_map(move |(y, img)| {
+            (0..w)
+                .filter(move |x| x % scale == 0)
+                .zip(repeat(img))
+                .map(move |(x, img)| get_pixel_char(pixel_brightness(img.get_pixel(x, y))))
+                .chain(['\n'])
+        })
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    read_image("test.jpg").unwrap();
+    let mut stdout = std::io::stdout().lock();
+    let img = ImageReader::open("image.png")?.decode()?.grayscale();
+    let res: String = image_ascii_chars(img, 80).collect();
+    write!(stdout, "{}", res)?;
     Ok(())
 }
